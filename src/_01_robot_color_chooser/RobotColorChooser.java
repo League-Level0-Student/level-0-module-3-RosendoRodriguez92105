1611d28d9289ef1cf8b4584a4a84dd4327128622
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-27 13:26:51 -0800

    cat_lady.ip

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,36 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		String color;
 		//1. Create a new Robot
-		
+	 Robot bob= new Robot("mini");
 		//3. Ask the user what color they would like the robot to draw
-		
+		      color= JOptionPane.showInputDialog(null, "Wut color do you you want");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+           if (color.equalsIgnoreCase("red") ) {
+           bob.penDown();
+           
         //6. If the user doesn’t enter anything, choose a random color
 
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+		for(int i=0;i<100;i++) {
 		//4. Set the pen width to 10
-		
+		   bob.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		   bob.setPenColor(1000000000,0,0);
+           bob.setSpeed(1000);
+           bob.move(100);
+           bob.turn(165);
+           bob.move(100);
+           bob.turn(185);
+		}
+	}
 	}
 }
--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,16 +5,23 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
+		String cats;
 		// 1. Ask the user how many cats they have
-
+          cats=JOptionPane.showInputDialog("how many cats do you have");
 		// 2. Convert their answer into an int
-
+int NumberOfCats= Integer.parseInt(cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+            if (NumberOfCats>=3) {
+            JOptionPane.showMessageDialog(null, "YOOOU Crazy LAdy");
+            }
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+            if (NumberOfCats<=3) {
+            	playVideo();
+            }
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 	}
